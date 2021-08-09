4724250f3810d5ec3d5235d3047e13359025d3f6
sverceks@gmail.com <sverceks@gmail.com>
2021-08-09 23:29:32 +0300

    New folder for lesson program file

---
+++ Lesson/FileControl.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+
+namespace Exer9_1
+{
+
+
+    [Serializable]
+    class FileControl
+    {
+        public static void SerializeToFile(Schedule schedule, string path)
+        {
+
+            BinaryFormatter formatter = new BinaryFormatter();
+
+            using (FileStream fs = new FileStream(path, FileMode.OpenOrCreate))
+            {
+                formatter.Serialize(fs, schedule);
+
+            }
+        }
+
+        public static Schedule DeserializeFromFile(string patch)
+        {
+            Schedule schedule = new Schedule();
+
+            BinaryFormatter formatter = new BinaryFormatter();
+
+            using (FileStream fs = new FileStream(patch, FileMode.OpenOrCreate))
+            {
+                schedule = (Schedule)formatter.Deserialize(fs);
+            }
+
+            return schedule;
+        }
+    }
+}
---
+++ Lesson/enumDayOfWeek.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+
+namespace Exer9_1
+{
+    [Serializable]
+    public enum DayOfWeek
+    {
+        Monday = 0,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday,
+        Saturday,
+        Sunday
+    }
+}
